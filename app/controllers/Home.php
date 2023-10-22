4104df158d826c66e2d7a34504f28c1a1dd66653
Muhammad Anggi Kusuma <raden@DESKTOP-K1JPJT5.com>
2023-10-23 00:05:47 +0700

    Tugas di Video 4

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,8 +1,11 @@
 <?php
 
-class Home {
-    public function page()
+class Home extends Controller {
+    public function index()
     {
-        echo 'home/page';
+        $data['judul'] = 'Home';
+        $this->view('templates/header', $data);
+        $this->view('home/index');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/home/index.php
@@ -0,0 +1,3 @@
+
+        <h1>Selamat Data di Webstite Saya</h1>
+    
