93cfc2cb786682eee3ee3ad471894f20fcabed76
Muhammad Anggi Kusuma <raden@DESKTOP-K1JPJT5.com>
2023-10-23 20:25:50 +0700

    Tugas di Video 7

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,25 +1,24 @@
 <?php
 
 class Mahasiswa_model {
-        private $dbh;
-        private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
+    
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
 
-        public function __construct()
-        {
-            // data source name
-            $dsn = 'mysql:host=localhost;dbname=prakweb2023_b_213040075_mvc';
+    public function getAllMahasiswa()
+    {
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
 
-            try {
-                $this->dbh = new PDO($dsn, 'root', '');
-            } catch(PDOException $e) {
-                die($e->getMessage());
-            }
-        }
-
-        public function getAllMahasiswa()
-        {
-            $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-            $this->stmt->execute();
-            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM mahasiswa WHERE id =:id;');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
 }
